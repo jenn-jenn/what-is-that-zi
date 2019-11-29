9075b10f8f9b4a109c157c194267a0aeee34d3d8
Jennifer Wong <jennwong1009@gmail.com>
2019-11-29 14:58:50 -0800

    insert characters onto flashcards

--- src/index.js
+++ src/index.js
@@ -4,16 +4,60 @@ let quiz = document.getElementById('quiz');
 let counter_span = document.getElementsByClassName('counter')[0];
 let countdown_div = document.getElementById('countdown');
 let overlay_div = document.getElementsByClassName('overlay')[0];
+let deckFlashcards_div = document.getElementsByClassName('deck')[1];
 
-let deck = [];
+console.log(deckFlashcards_div);
+
+const deck = [];
+
+function createFlashcards() {
+    let char, def, pinyin, newCard, front, back, character, p, charID;
+    let img = document.createElement('img');
+    img.src = '/img/backface.png';
+    for (let i = 0; i < 10; i++) {
+        char = deck[i].character;
+        def = deck[i].definition;
+        pinyin = deck[i].pinyin;
+        newCard = document.createElement('div');
+        newCard.className = 'flashcard';
+
+        front = document.createElement('div');
+        front.className = 'front';
+
+        back = document.createElement('div');
+        back.className = 'back';
+        character = document.createElement('div');
+        charID = `character-${i}`;
+        character.id = charID;
+            
+        let writer = HanziWriter.create(character, char, {
+            width: 20, height: 20, padding: 1
+        });
+        p = document.createElement('p');
+        p.className = 'definition'
+        front.appendChild(character);
+        front.appendChild(p);
+        back.appendChild(img);  
+        newCard.appendChild(front);
+        newCard.appendChild(back);
+        deckFlashcards_div.append(newCard);
+    }
+    
+}
+
 
 function getRandom() {
+    let randomI, char;
     for(let i = 0; i < 10; i++) {
-        let char = characters[Math.floor(Math.random() * characters.length)];
-        if(deck.indexOf(char) == -1) {
+        randomI = Math.floor(Math.random() * characters.length);
+        char = characters[randomI];
+        if(deck.indexOf(char) === -1) {
             deck.push(char);
+        } else {
+            i--;
         }
     }
+    createFlashcards();
 } 
 
 function showQuiz() {
@@ -35,8 +79,10 @@ function decreaseCounter(callback, delay, reps) {
             setTimeout(function() {
                 counter_span.classList.add('hidden');
                 overlay_div.style.display = 'none';
+                countdown_div.style.display = 'none';
             }, 1000);
-            
+            // showQuiz();
+            // showCards();
         }
     }, delay);
     
@@ -56,7 +102,6 @@ function hideDirectionsAndStart() {
         directions_div.classList.add('hidden');
         start_btn.classList.add('hidden');
         countdown_div.style.display = 'flex';
-        overlay_div.classList.add('fadein')
         overlay_div.classList.remove('hidden');
         startCountdown();
     }, 1000);
@@ -66,6 +111,7 @@ function hideDirectionsAndStart() {
 start_btn.addEventListener('click', function() {
     getRandom();
     console.log('clicked start');
+    console.log(deck);
     start_btn.disabled = 'disabled';
     setTimeout(hideDirectionsAndStart, 500);
 })
