74c319ad47c091dd620ad3f3fd728945b69676f6
Jennifer Wong <jennwong1009@gmail.com>
2019-11-29 10:52:15 -0800

    fade in fade out quiz and directions

--- src/card.js
+++
@@ -1,5 +0,0 @@
-function Card(char) {
-    this.char = char.character;
-    this.pinyin = char.pinyin;
-    this.def = char.definition;
-}
--- src/index.js
+++ src/index.js
@@ -1,50 +1,42 @@
-const Game = require('./quiz');
-
-window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('flashcards ready to go');
-    let quiz = new quiz();
-    quiz.start();
-    console.log(quiz.deck);
-});
+let start_btn = document.getElementById('start-btn');
+let directions_div = document.getElementById('directions');
+let quiz = document.getElementById('quiz');
 
 
+let deck = [];
+
+function getRandom() {
+    for(let i = 0; i < 10; i++) {
+        let char = characters[Math.floor(Math.random() * characters.length)];
+        if(deck.indexOf(char) == -1) {
+            deck.push(char);
+        }
+    }
+} 
+
+function showQuiz() {
+    quiz.classList.add('fadein');
+    setTimeout(function(){
+        quiz.classList.remove('hidden');
+    }, 1000);
+}
 
 
+function hideDirectionsAndStart() {
+    console.log("hiding..");
+    directions_div.classList.add('fadeout');
+    start_btn.classList.add('fadeout');
+    setTimeout(function(){
+        directions_div.classList.add('hidden');
+        start_btn.classList.add('hidden');
+    }, 500);
+    showQuiz();
+}
+ 
 
-// import { getRandom } from '../characters/characters';
+start_btn.addEventListener('click', function() {
+    getRandom();
+    console.log('clicked start');
+    setTimeout(hideDirectionsAndStart, 500);
+})
 
-// var directions_div = document.getElementById('directions');
-// var start_btnn = document.getElementsByClassName('start-btn')[0];
-// var quiz_div = document.getElementById('quiz');
-
-// function clickStart() {
-//     directions_div.classList.add('fadeout');
-//     start_btnn.classList.add('fadeout');
-//     setTimeout(function() {
-//         shuffle();
-//         showQuiz();
-//     }, 500);
-// }
-
-// function clickRestart() {
-    
-//     setTimeout(function() {
-//         quiz_div.classList.remove('fadein');
-        
-//     }, 500);
-
-// }
-
-// function showQuiz() {
-//     directions_div.classList.add('hidden');
-//     start_btnn.classList.add('hidden');
-//     quiz_div.classList.remove('hidden');
-
-//     setTimeout(function() {
-//         quiz_div.classList.add('fadein');
-//     }, 1000);
-// }
-
-// function shuffle() {
-//     var 
-// }
